2d3a9d0df8dcc726c313ab9b5b547272fedf0953
AdelArab <a.arab@lancaster.ac.uk>
2026-09-23 21:37:17 +0100

    Clean repository structure for portfolio

--- SnowProblem_Part1/Part1/src/snowproblem/SnowProblem_Task1/src/snowproblem/model/PieceType.java
+++
@@ -1,9 +0,0 @@
-package snowproblem.model;
-
-/** The different piece types that can appear on the board. */
-public enum PieceType {
-    TREE,
-    LARGE_SNOWBALL,
-    SMALL_SNOWBALL,
-    SNOWMAN_HEAD
-}
